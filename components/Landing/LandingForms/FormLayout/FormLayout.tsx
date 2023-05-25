e9068172f60715ba925ebb6f6d6d13f966274bed
jarji <100996174+jarjia@users.noreply.github.com>
2023-05-25 18:33:44 +0400

    fix: dont render layout if its receiving empty child element

--- components/Landing/LandingForms/FormLayout/FormLayout.tsx
+++ components/Landing/LandingForms/FormLayout/FormLayout.tsx
@@ -5,8 +5,14 @@ const FormLayout: React.FC<FormLayoutTypes> = ({
   handleFormStatus,
   children,
 }) => {
+  console.log(children.type);
+
   return (
-    <div className='flex justify-center items-center fixed z-[99] backdrop-blur-landing-blur w-screen h-screen'>
+    <div
+      className={`${
+        children.type === 'section' ? 'hidden' : 'flex'
+      } justify-center items-center fixed z-[99] backdrop-blur-landing-blur w-screen h-screen`}
+    >
       <div className='w-[35%] large:w-[25%] h-auto sm:w-full sm:h-full rounded-form-radius bg-form-back sm:px-10 px-14 py-8'>
         <div className='float-right relative bottom-5 left-5 sm:left-2'>
           <div
