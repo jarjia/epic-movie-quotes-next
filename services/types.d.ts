a596be1982e4ce11d94809301c32d94b5b0f02d7
jarji <100996174+jarjia@users.noreply.github.com>
2023-05-25 18:36:09 +0400

    fix: remove unneccesary undefined type

--- services/types.d.ts
+++ services/types.d.ts
@@ -12,6 +12,6 @@ export type PostRecoverEmailTypes = {
 };
 export type PostRecoverPasswordTypes = {
   password: string;
-  email: string | string[] | undefined;
-  token: string | string[] | undefined;
+  email: string | string[];
+  token: string | string[];
 };
