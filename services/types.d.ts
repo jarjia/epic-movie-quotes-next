f38ea2c346a9f6558d8961f47e7b80ef519f7504
jarji <100996174+jarjia@users.noreply.github.com>
2023-05-25 12:12:29 +0400

    feat: add new services

--- services/types.d.ts
+++ services/types.d.ts
@@ -1,5 +1,17 @@
-export type postRegisterTypes = {
+export type PostRegisterTypes = {
   name: string;
   email: string;
   password: string;
+};
+export type PostVerifyTypes = {
+  email: string | string[];
+  token: string | string[];
+};
+export type PostRecoverEmailTypes = {
+  email: string;
+};
+export type PostRecoverPasswordTypes = {
+  password: string;
+  email: string | string[] | undefined;
+  token: string | string[] | undefined;
 };
