06fe9e2ed05a779f25abf6af7e2e899629b3ee3f
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-02 20:22:29 +0400

    feat: add new form components for news feed page

--- components/Forms/index.ts
+++ components/Forms/index.ts
@@ -1,3 +1,7 @@
 export * from './Input';
 export * from './Checkbox';
 export * from './PasswordInput';
+export * from './FeedFormLayout';
+export * from './FeedTextarea';
+export * from './FileInput';
+export * from './SelectMovie';
