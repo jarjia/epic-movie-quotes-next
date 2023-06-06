38811db6dffac9e867696ee6aca61efb16d02e2a
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-06 17:21:55 +0400

    chore: remove unnecessary comment

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -4,13 +4,4 @@ const Profile = () => {
   return <UserProfile />;
 };
 
-// export const getServerSideProps = async () => {
-//   await axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie');
-//   const res = await axios.get('http://127.0.0.1:8000/api/bla', {
-//     withCredentials: true,
-//   });
-//   const data = res.data;
-//   return { props: { data } };
-// };
-
 export default Profile;
