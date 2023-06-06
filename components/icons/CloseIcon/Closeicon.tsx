ed5d6fd2f4a46579910177ef2edad9585275f16f
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-06 13:07:25 +0400

    feat: make close icon change color

--- components/icons/CloseIcon/Closeicon.tsx
+++ components/icons/CloseIcon/Closeicon.tsx
@@ -1,11 +1,12 @@
-const Closeicon: React.FC = () => {
+const Closeicon: React.FC<{ color: boolean }> = (props) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       viewBox='0 0 24 24'
       width='24px'
       height='24px'
-      fill='white'
+      className='rotate-45'
+      fill={props.color ? '#000000' : 'white'}
       fillRule='evenodd'
     >
       <path
