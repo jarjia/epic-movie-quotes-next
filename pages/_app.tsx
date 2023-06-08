f684db3a260a5dd2b46f4ca0e58658c84cd43850
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-08 13:59:16 +0400

    feat: add title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import type { AppProps } from 'next/app';
 import { AppContextProvider } from '@/context';
+import Head from 'next/head';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,10 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Movie Quotes</title>
+        <link rel='icon' href='/assets/images/favicon.png' />
+      </Head>
       <AppContextProvider>
         <MyComponent {...pageProps} />
       </AppContextProvider>
