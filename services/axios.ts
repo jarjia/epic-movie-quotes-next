8ac1c00d4824d1a2b22d379cd058fcccf3fcd47d
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-09 20:44:31 +0400

    feat: send current locale for laravel

--- services/axios.ts
+++ services/axios.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
+let locale;
+if (typeof window !== 'undefined') {
+  locale =
+    localStorage.getItem('locale') === null
+      ? 'en'
+      : localStorage.getItem('locale');
+}
+
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
+  params: { locale },
 });
 
 instance.defaults.withCredentials = true;
