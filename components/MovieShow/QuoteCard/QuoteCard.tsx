73e49bfae20a282b7b9676998b950439a8cdfdec
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-14 19:57:29 +0400

    feat: implment dynamic quote cards

--- components/MovieShow/QuoteCard/QuoteCard.tsx
+++ components/MovieShow/QuoteCard/QuoteCard.tsx
@@ -6,9 +6,25 @@ import {
   TrashBinIcon,
 } from '@/components';
 import useQuoteCard from './useQuoteCard';
+import { QuoteCardTypes } from './types';
 
-const QuoteCard = () => {
-  const { isBox, handleSetIsBox, t } = useQuoteCard();
+const QuoteCard: React.FC<QuoteCardTypes> = ({
+  thumbnail,
+  quote,
+  id,
+  likesNumber,
+  commentsNumber,
+  handleRefecthQuotes,
+}) => {
+  const {
+    isBox,
+    handleSetIsBox,
+    t,
+    locale,
+    handleFeedFormStatus,
+    deleteQuoteMutate,
+    handleCurrentQuoteId,
+  } = useQuoteCard(handleRefecthQuotes);
 
   return (
     <div className='bg-post-bg pt-4 px-5 rounded-form-radius'>
@@ -22,24 +38,43 @@ const QuoteCard = () => {
         <div
           className='w-full mt-3 h-[100px] large:h-[130px] bg-cover rounded-[2px]'
           style={{
-            backgroundImage:
-              'url(https://i2-prod.chroniclelive.co.uk/incoming/article20915599.ece/ALTERNATES/s615b/0_BILLY-ELLIOT.jpg)',
+            backgroundImage: `url(${thumbnail})`,
           }}
         ></div>
-        <div className='flex flex-col justify-center items-center w-full h-full pr-8'>
-          <div className='flex justify-end w-full relative left-4 mb-1'>
+        <div className='grid grid-cols-1 grid-rows-[5%_95%] w-full h-full pr-8'>
+          <div className='flex relative justify-end w-full left-4 mb-1'>
             {isBox && (
               <div className='relative top-4 right-8 z-[999]'>
-                <div className='flex flex-col absolute bg-add-quote-bg rounded-form-radius gap-4 p-6 w-[200px]'>
-                  <button className='flex w-full items-center text-white gap-2'>
+                <div className='flex flex-col sm:right-[-50px] absolute bg-add-quote-bg rounded-form-radius gap-4 p-6 w-[200px]'>
+                  <button
+                    onClick={() => {
+                      handleSetIsBox();
+                      handleCurrentQuoteId(String(id));
+                      handleFeedFormStatus('view-quote');
+                    }}
+                    className='flex w-full items-center text-white gap-2'
+                  >
                     <EyeIcon />
                     <p>{t('view_quote')}</p>
                   </button>
-                  <button className='flex w-full items-center text-white gap-2'>
+                  <button
+                    onClick={() => {
+                      handleSetIsBox();
+                      handleCurrentQuoteId(String(id));
+                      handleFeedFormStatus('edit-quote');
+                    }}
+                    className='flex w-full items-center text-white gap-2'
+                  >
                     <PenIcon />
                     <p>{t('edit_quote')}</p>
                   </button>
-                  <button className='flex w-full items-center text-white gap-2'>
+                  <button
+                    onClick={() => {
+                      deleteQuoteMutate(String(id));
+                      handleSetIsBox();
+                    }}
+                    className='flex w-full items-center text-white gap-2'
+                  >
                     <TrashBinIcon />
                     <p>{t('delete_quote')}</p>
                   </button>
@@ -52,23 +87,22 @@ const QuoteCard = () => {
               <span className='w-[5px] h-[5px] mx-0.5 bg-white rounded-full'></span>
             </button>
           </div>
-          <p className='flex mt-2 items-center text-placeholder max-h-[100px] text-xl'>
+          <p className='flex pl-4 items-center text-placeholder max-h-[100px] text-xl'>
             <span className='line-clamp-3 break-all'>
-              &quot;You dont understand! I coulda had class. I coulda been a
-              contender. I couldve been somebody,&quot;
+              &quot;{quote[locale]}&quot;
             </span>
           </p>
         </div>
       </div>
       <div className='flex py-4 gap-6'>
-        <button className='flex gap-2 text-white'>
-          7
+        <div className='flex gap-2 text-white'>
+          {commentsNumber}
           <CommentIcon />
-        </button>
-        <button className='flex gap-2 text-white'>
-          10
-          <HeartIcon />
-        </button>
+        </div>
+        <div className='flex gap-2 text-white'>
+          {likesNumber}
+          <HeartIcon hasLiked={false} />
+        </div>
       </div>
     </div>
   );
