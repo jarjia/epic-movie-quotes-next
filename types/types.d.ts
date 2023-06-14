030377b500a9412ea51c346cea80bc0a1a0da1cc
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-14 19:51:15 +0400

    feat: add new global types

--- types/types.d.ts
+++ types/types.d.ts
@@ -72,3 +72,75 @@ export type GenreObjectType = {
   id: number;
   genre: Keys;
 };
+export type MovieShowTypes = {
+  created_at: string;
+  description: Keys;
+  director: Keys;
+  movie: Keys;
+  id: number;
+  genres: [
+    {
+      id: number;
+      genre: Keys;
+    }
+  ];
+  releaseDate: string;
+  thumbnail: string;
+  updated_at: string;
+  user_id: number;
+};
+export type QuotesTypes = {
+  id: number;
+  quote: Keys;
+  thumbnail: string;
+  movie_id: number;
+  comments: {
+    id: number;
+    comment: string;
+    user: {
+      id: number;
+      name: string;
+      thumbnail: string;
+    };
+  }[];
+  likes: {
+    id: number;
+    user: {
+      id: number;
+    };
+  }[];
+};
+export type UpdateQuotesTypes = {
+  quote: Keys;
+  movie_id: number;
+  thumbnail: string;
+};
+export type PostsTypes = {
+  id: number;
+  quote: Keys;
+  thumbnail: string;
+  movie_id: number;
+  movies: {
+    movie: Keys;
+    thumbnail: string;
+    releaseDate: string;
+    user: {
+      name: string;
+      thumbnail: string;
+    };
+  };
+  comments: {
+    id: number;
+    comment: string;
+    user: {
+      name: string;
+      thumbnail: string;
+    };
+  }[];
+  likes: {
+    id: number;
+    user: {
+      id: number;
+    };
+  }[];
+};
