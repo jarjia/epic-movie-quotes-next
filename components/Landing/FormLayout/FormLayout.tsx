1ecb45b0fc59cc66b02edf41e659e34d7c03b233
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-26 19:41:55 +0400

    feat: make modal scrollable if overflown

--- components/Landing/FormLayout/FormLayout.tsx
+++ components/Landing/FormLayout/FormLayout.tsx
@@ -2,20 +2,33 @@ import { CloseIcon } from '@/components';
 import { FormLayoutTypes } from './types';
 import { useContext } from 'react';
 import { AppContext } from '@/context';
+import { useAuthService } from '@/services';
+import { useQuery } from 'react-query';
 
 const FormLayout: React.FC<FormLayoutTypes> = ({
   handleFormStatus,
   children,
 }) => {
   const { handleFeedFormStatus } = useContext(AppContext);
+  const { getCrsfToken } = useAuthService();
+  useQuery('csrf-token', getCrsfToken);
 
   return (
     <div
+      onClick={() => {
+        handleFormStatus('null');
+        handleFeedFormStatus('');
+      }}
       className={`${
         children.type === 'section' ? 'hidden' : 'flex'
-      } justify-center items-center fixed z-[99] backdrop-blur-landing-blur w-screen h-screen`}
+      } justify-center drop-shadow-none items-center fixed z-[99] backdrop-blur-landing-blur w-screen h-screen`}
     >
-      <div className='w-[35%] large:w-[25%] h-auto sm:w-full sm:h-full rounded-form-radius bg-form-back sm:px-10 px-14 py-8'>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className='w-[35%] mid:w-1/2 large:w-[25%] h-auto sm:w-full sm:h-full sm:rounded-none rounded-form-radius bg-form-back sm:px-10 px-14 py-8'
+      >
         <div className='float-right relative bottom-5 left-5 sm:left-2'>
           <div
             onClick={() => {
@@ -27,7 +40,7 @@ const FormLayout: React.FC<FormLayoutTypes> = ({
             <CloseIcon color={false} isSmall={false} />
           </div>
         </div>
-        {children}
+        <div className='h-full overflow-y-scroll scrollbar'>{children}</div>
       </div>
     </div>
   );
