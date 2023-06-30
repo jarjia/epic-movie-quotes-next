cd68edc5091344826ea3db5a4c98af35ff0fc068
jarji <100996174+jarjia@users.noreply.github.com>
2023-06-30 13:28:28 +0400

    feat: show file upload state

--- components/shared/ReplacePhoto/ReplacePhoto.tsx
+++ components/shared/ReplacePhoto/ReplacePhoto.tsx
@@ -13,7 +13,7 @@ const ReplacePhoto: React.FC<{ movieImage: string }> = (props) => {
         className='grid grid-cols-2 min-h-[130px] border-[1px] mt-4 border-placeholder'
       >
         <div
-          className='w-full bg-cover h-full'
+          className='w-full bg-horizon-center bg-cover h-full'
           style={{
             backgroundImage: `url(${img === null ? props.movieImage : img})`,
           }}
