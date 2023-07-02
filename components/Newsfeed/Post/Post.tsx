f9fc8d67020f8f480209a8ac646640452139f50b
jarji <100996174+jarjia@users.noreply.github.com>
2023-07-02 18:01:15 +0400

    feat: make post height dynamic to its width

--- components/Newsfeed/Post/Post.tsx
+++ components/Newsfeed/Post/Post.tsx
@@ -3,7 +3,7 @@ import { PostTypes } from '@/types';
 import usePost from './usePost';
 
 const Post: React.FC<PostTypes> = (props) => {
-  const { locale, t } = usePost();
+  const { locale, t, postHeight, postRef } = usePost();
 
   return (
     <div className='text-white text-xl bg-post-bg rounded-xl sm:p-8 p-4 my-8'>
@@ -25,9 +25,11 @@ const Post: React.FC<PostTypes> = (props) => {
         </div>
       </div>
       <div
-        className='w-full h-[400px] sm-mid:h-[250px] bg-cover bg-white rounded-form-radius'
+        ref={postRef}
+        className='w-full bg-cover bg-white rounded-form-radius'
         style={{
           backgroundImage: `url(${props.thumbnail})`,
+          height: `${postHeight}px`,
         }}
       ></div>
       <PostController data={props} userId={props.user_id} />
--- components/Newsfeed/Post/usePost.ts
+++ components/Newsfeed/Post/usePost.ts
@@ -1,14 +1,38 @@
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const usePost = () => {
   const { t } = useTranslation('newsFeed');
   const router = useRouter();
   let locale = router.locale as string;
+  const [postHeight, setPostheight] = useState<number | null>(null);
+  const postRef = useRef<null | HTMLDivElement>(null);
+  const percent = 60;
+
+  const handlePostWidth = useCallback(() => {
+    if (postRef.current) {
+      let height = (percent / 100) * postRef.current.offsetWidth;
+      setPostheight(Math.floor(height));
+    }
+  }, [setPostheight]);
+
+  useEffect(() => {
+    if (postRef.current) {
+      let height = (percent / 100) * postRef.current.offsetWidth;
+      setPostheight(Math.floor(height));
+    }
+    window.addEventListener('resize', handlePostWidth);
+    return () => {
+      window.addEventListener('resize', handlePostWidth);
+    };
+  }, [handlePostWidth]);
 
   return {
     locale,
+    postHeight,
     t,
+    postRef,
   };
 };
 
