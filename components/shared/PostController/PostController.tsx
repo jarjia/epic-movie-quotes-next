a5f092daee06e6032e448c15f14244a79836acb5
jarji <100996174+jarjia@users.noreply.github.com>
2023-07-05 21:10:35 +0400

    feat: remove comment follow functionality

--- components/shared/PostController/PostController.tsx
+++ components/shared/PostController/PostController.tsx
@@ -11,13 +11,10 @@ const PostController: React.FC<{ data: PostTypes; userId: number }> = ({
     openComments,
     likedIds,
     handleOpenComments,
-    lastComment,
-    followComments,
     handleSubmit,
     handleLiked,
     filteredComments,
     searchRef,
-    commentRef,
     comments,
     setOpenComments,
     isLoading,
@@ -32,7 +29,7 @@ const PostController: React.FC<{ data: PostTypes; userId: number }> = ({
 
   return (
     <>
-      <div ref={commentRef} className='flex py-4 gap-6'>
+      <div className='flex py-4 gap-6'>
         <button onClick={handleOpenComments} className='select-none flex gap-2'>
           {comments.length}
           <CommentIcon />
@@ -47,47 +44,36 @@ const PostController: React.FC<{ data: PostTypes; userId: number }> = ({
         </button>
       </div>
       <div className='w-full h-[1px] bg-search-bar-border'></div>
-      {openComments > 0 && (
+      {openComments > 0 && filteredComments.length > 0 && (
         <div className='py-4 sm:py-2'>
-          {comments.length === 0 ? (
-            <p className='text-center'>{t('no_comments')}</p>
-          ) : (
-            filteredComments.map((comment, index) => {
-              const isLastComment = index === filteredComments.length - 1;
-
-              return (
-                <div
-                  ref={isLastComment ? lastComment : null}
-                  key={comment.id}
-                  className='sm:pt-2'
-                >
-                  <div className='flex items-center'>
-                    <div
-                      className='w-profile h-profile rounded-full bg-center bg-cover'
-                      style={{
-                        backgroundImage: `url(${comment.user.thumbnail})`,
-                      }}
-                    ></div>
-                    <p className='pl-4 break-words capitalize'>
-                      {comment.user.name}
-                    </p>
-                  </div>
-                  <div className='pl-[74px] sm:pl-0 sm:pt-2'>
-                    <p className='text-base break-words pb-3 border-b-[1px] border-search-bar-border'>
-                      {comment.comment}
-                    </p>
-                  </div>
+          {filteredComments.map((comment) => {
+            return (
+              <div key={comment.id} className='sm:pt-2'>
+                <div className='flex items-center'>
+                  <div
+                    className='w-profile h-profile rounded-full bg-center bg-cover'
+                    style={{
+                      backgroundImage: `url(${comment.user.thumbnail})`,
+                    }}
+                  ></div>
+                  <p className='pl-4 break-words capitalize'>
+                    {comment.user.name}
+                  </p>
                 </div>
-              );
-            })
-          )}
+                <div className='pl-[74px] sm:pl-0 sm:pt-2'>
+                  <p className='text-base break-words pb-3 border-b-[1px] border-search-bar-border'>
+                    {comment.comment}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
           {comments.length !== 0 && (
             <div className='flex justify-between items-center'>
               {openComments >= comments.length ? null : (
                 <button
                   onClick={() => {
                     setOpenComments((prev) => prev + 2);
-                    followComments();
                   }}
                   className='pl-[74px] sm:pl-0 mt-2 text-white underline text-md'
                 >
@@ -104,12 +90,13 @@ const PostController: React.FC<{ data: PostTypes; userId: number }> = ({
           )}
         </div>
       )}
-      <div className='flex items-center mt-2 sm:pb-2'>
+      <div className='flex items-center mt-4 sm:pb-2'>
         <UserProfile />
         <form className='w-full ml-3' onSubmit={handleSubmit}>
           <input
+            ref={searchRef}
             type='text'
-            className='w-full rounded-form-radius pl-4 placeholder-input pb-2 caret-white text-white border-0 bg-add-quote-bg focus:ring-0 focus:border-transparent'
+            className='w-full rounded-form-radius pl-4 h-[52px] placeholder-input caret-white text-white border-0 bg-add-quote-bg focus:ring-0 focus:border-transparent'
             placeholder={`${t('comment_placeholder')}`}
             disabled={isLoading}
           />
