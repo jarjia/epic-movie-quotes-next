6c9730b204e1ec6d3cb1cb3f8e10c1c22f52276d
jarji <100996174+jarjia@users.noreply.github.com>
2023-09-09 19:56:13 +0400

    chore: clear

--- pages/newsfeed.tsx
+++ pages/newsfeed.tsx
@@ -1,58 +1,24 @@
 import { Feed } from '@/components';
 import { GetServerSidePropsContext } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { getData } from './api/getUser';
 
-const NewsFeed = (props: any) => {
-  console.log(props);
-
+const NewsFeed = () => {
   return <Feed />;
 };
 
 export async function getServerSideProps({
   locale,
 }: GetServerSidePropsContext) {
-  try {
-    await getData();
-
-    return {
-      props: {
-        ...(await serverSideTranslations(locale as string, [
-          'common',
-          'formErrors',
-          'newsFeed',
-          'apiErrors',
-        ])),
-      },
-    };
-  } catch (error) {
-    return {
-      redirect: {
-        destination: '/403',
-        permanent: false,
-      },
-    };
-  }
+  return {
+    props: {
+      ...(await serverSideTranslations(locale as string, [
+        'common',
+        'formErrors',
+        'newsFeed',
+        'apiErrors',
+      ])),
+    },
+  };
 }
 
 export default NewsFeed;
-
-// try {
-//   await axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/user`);
-
-//   return {
-//     props: {
-//       ...(await serverSideTranslations(locale as string, [
-//         'common',
-//         'formErrors',
-//         'newsFeed',
-//         'apiErrors',
-//       ])),
-//     },
-//   };
-// } catch (error) {
-//   res.writeHead(302, {
-//     Location: '/403',
-//   });
-//   res.end();
-// }
