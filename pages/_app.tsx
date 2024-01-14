fb983a9922b609b12ad2f7c7c2a955c85fb1d488
jarji <100996174+jarjia@users.noreply.github.com>
2024-01-14 19:00:33 +0400

    feat: make movie quotes pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       <Head>
         <title>Movie Quotes</title>
         <link rel='icon' href='/assets/images/favicon.png' />
+        <link rel='manifest' href='/manifest.json' />
       </Head>
       <AppContextProvider>
         <MyComponent {...pageProps} />
